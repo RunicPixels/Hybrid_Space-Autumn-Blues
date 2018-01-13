5ac191d988d3e8d9b0c0ad4256bf790fe95f836e
b <bas.de.waal@hotmail.com>
2018-01-13 15:40:46 +0100

    o

--- Assets/Final/StartBreading.cs
+++ Assets/Final/StartBreading.cs
@@ -13,6 +13,11 @@ public class StartBreading : MonoBehaviour {
     int fadeDir = -1;
     public int curretScene = 0;
 
+    public List<Animator> allAniInScene = new List<Animator>();
+    public float startCoundown = 4;
+    public float currentCoundown;
+    public Texture2D color;
+     
     void Awake(){
         DontDestroyOnLoad(this);
     }
@@ -26,28 +31,38 @@ public class StartBreading : MonoBehaviour {
         }
 
         if (Input.GetKeyDown("1")){
-            //SceneManager.LoadScene(1+1);
             curretScene = 2;
             StartCoroutine(Fade());
         }
         if (Input.GetKeyDown("2")){
-            //SceneManager.LoadScene(2+1);
             curretScene = 3;
             StartCoroutine(Fade());
         }
-
+        if (currentCoundown > 0){
+            currentCoundown = currentCoundown - Time.deltaTime;
+        }
+        else{
+            StartCoroutine(StartAni(true));
+        }
     }
 
     IEnumerator Fade(){
         int c = curretScene;
-        curretScene = SceneManager.GetActiveScene().buildIndex;
         float fadeTime = BeginFade(1); 
         yield return new WaitForSeconds(fadeTime);
         SceneManager.LoadScene(c);
     }
 
+    void StartTimer(){
+        currentCoundown = startCoundown;
+    }
+
     private void OnGUI(){
-        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        if (currentCoundown > 0){
+            GUI.DrawTexture(new Rect(0, Screen.height - 10, Screen.width*(currentCoundown/startCoundown), 10), color);
+        }
+
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;  
         alpha = Mathf.Clamp01(alpha);
         GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
         GUI.depth = drawDepth;
@@ -62,9 +77,26 @@ public class StartBreading : MonoBehaviour {
 
     private void OnLevelWasLoaded(int level){
         BeginFade(-1);
-
+        StartTimer();
+        StartCoroutine(StartAni(false));
     }
 
+    IEnumerator StartAni(bool b){
+        yield return new WaitForEndOfFrame();
+        for (int i = 0; i < allAniInScene.Count; i++){
+            allAniInScene[i].enabled = b;
+        }
+
+        /*
+        object[] an = FindObjectsOfType(typeof(Animator));
+        yield return new WaitForEndOfFrame();
+        for (int i = 0; i < an.Length; i++){
+            Debug.Log(an.Length);
+            Animator a = an[i] as Animator;
+            Debug.Log(a);
+            a.enabled = b;
+        }*/
+    }
 
 }
 /*
