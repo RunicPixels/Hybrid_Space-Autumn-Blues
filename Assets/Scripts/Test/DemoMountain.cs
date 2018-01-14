980f864466d672ae2a951bb466755e1e3a6790c1
Cheezegami <superronald@live.nl>
2018-01-14 23:51:51 +0100

    Supeur Beautified Scene 2.

--- Assets/Scripts/Test/DemoMountain.cs
+++ Assets/Scripts/Test/DemoMountain.cs
@@ -5,49 +5,50 @@ using UnityEngine;
 public class DemoMountain : Sequence {                             
     public Vector3 startSize = new Vector3(1 ,1 ,1 );                                                   // Scale to start your sprite
     public Vector3 endSize = new Vector3(3, 5, 3);                                                      // Scale of your sprite at the end of the sequence.
-    public float distCovered = 0;                                                                       // Amount of progress between 0 and 1.
     public SpriteRenderer[] renderers;                                                                  // The Sprite Renderer that visualises the sprite / animation
     public MountainAnimations[] mountainAnimations;
     public GameObject[] stars;
+
+    
 
     public Gradient skyGradient;
     private Skybox skybox;
     private Material skyMaterial;
     public Color skyColour;
 
+    private float camBaseZPos;
+    public float camZPosModifier = 10;
+    private float camXRot;
+    public float camXRotModifier = 5;
 
     public override void Start() {
         base.Start();
         skybox = Camera.main.GetComponent<Skybox>();
         skyMaterial = skybox.material;
+        camBaseZPos = mainCamera.transform.position.z;
+        camXRot = mainCamera.transform.rotation.eulerAngles.x;
     }
 
 
 	// Update is called once per frame
-	void Update () {
-        if(Input.GetKeyDown(KeyCode.Space) && growth == false) {                                        // Iniatates growth when space is pressed.
-            growth = true;
-            Invoke("StopGrowth", 1f);                                                                   // Stops Growth after 1 second.
-        }
-
-        if (growth) {
-            distCovered += Time.deltaTime * speed;                                                       // Adds growth to the sequence that is divided by speed seconds (I.E. With 0.5 speed you need to have your hand in the growth area for two seconds. with 0.02 it's 50 seconds ( 0.02 * 50 = 1))
-            if(distCovered >= 1) {                                                                       // Small hack to prevent sprites from overflowing.
-                distCovered = 0.999f;
-            }
-            stars[Mathf.FloorToInt(stars.Length * distCovered)].SetActive(true);
-
-            for(int i = 0; i< renderers.Length; i++) {
-            renderers[i].sprite = mountainAnimations[i].animationFrames[Mathf.FloorToInt(mountainAnimations[i].animationFrames.Length * distCovered)];  // Change sprite based on point in sequence. (I.E. With 5 sprites it changes to the next one every 1 / 5 = 0.2 so it will change sprites at 0.2 , 0.4, 0.6, 0.8 etc in a linear fashion.
-            }
-        }
-        float fracJourney = distCovered;                                                                // Current Point in the sequence.
-        transform.localScale = Vector3.Lerp(startSize, endSize, fracJourney);                           // Change Size
-        skyColour = skyGradient.Evaluate(fracJourney);
+	public override void Update () {
+        base.Update();
+        transform.localScale = Vector3.Lerp(startSize, endSize, distCovered); // Change Size 
+        skyColour = skyGradient.Evaluate(distCovered);
         skyMaterial.SetColor("_Tint", skyColour);
 	}
 
-    void StopGrowth() {
-        growth = false;
+    public override void Grow() {
+        base.Grow();
+        stars[Mathf.FloorToInt(stars.Length * distCovered)].SetActive(true);
+
+        for (int i = 0; i < renderers.Length; i++) {
+            renderers[i].sprite = mountainAnimations[i].animationFrames[Mathf.FloorToInt(mountainAnimations[i].animationFrames.Length * distCovered)];  // Change sprite based on point in sequence. (I.E. With 5 sprites it changes to the next one every 1 / 5 = 0.2 so it will change sprites at 0.2 , 0.4, 0.6, 0.8 etc in a linear fashion.
+        }
+        mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, camBaseZPos + (camZPosModifier * distCovered));
+
+        float angle = (distCovered * camXRotModifier) + camXRot;
+        mainCamera.transform.rotation = Quaternion.AngleAxis(angle, Vector3.left);
     }
+
 }
--- Assets/Scripts/Test/DemoTree.cs
+++ Assets/Scripts/Test/DemoTree.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,38 +6,26 @@ public class DemoTree : Sequence {
     public Sprite[] animationFrames; // Animation Frames that looped through in a linear fashion.
     public Vector3 startSize = new Vector3(1, 1, 1); // Scale to start your sprite
     public Vector3 endSize = new Vector3(3, 5, 3); // Scale of your sprite at the end of the sequence.
-    float distCovered = 0; // Amount of progress between 0 and 1.
     public SpriteRenderer renderer;
-    private float zoom;
-    public float zoomScale = 20;
     // Use this for initialization
 
     public override void Start() {
         renderer = GetComponent<SpriteRenderer>(); // The Sprite Renderer that visualises the sprite / animation
         base.Start();
-        zoom = camera.GetComponent<Camera>().fieldOfView;
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && growth == false) { // Iniatates growth when space is pressed.
-            growth = true;
-            Invoke("StopGrowth", 1f); // Stops Growth after 1 second.
-        }
-
-        if (growth) {
-            distCovered += Time.deltaTime * speed; // Adds growth to the sequence that is divided by speed seconds (I.E. With 0.5 speed you need to have your hand in the growth area for two seconds. with 0.02 it's 50 seconds ( 0.02 * 50 = 1))
-            if (distCovered >= 1) { // Small hack to prevent sprites from overflowing.
-                distCovered = 0.999f;
-            }
-            renderer.sprite = animationFrames[Mathf.FloorToInt(animationFrames.Length * distCovered)]; // Change sprite based on point in sequence. (I.E. With 5 sprites it changes to the next one every 1 / 5 = 0.2 so it will change sprites at 0.2 , 0.4, 0.6, 0.8 etc in a linear fashion.
-            camera.GetComponent<Camera>().fieldOfView = zoom + (distCovered * zoomScale);
-        }
-        float fracJourney = distCovered; // Current Point in the sequence.
-        transform.localScale = Vector3.Lerp(startSize, endSize, fracJourney); // Change Size
+    public override void Update() {
+        base.Update();
+        transform.localScale = Vector3.Lerp(startSize, endSize, distCovered); // Change Size
     }
 
     void StopGrowth() {
         growth = false;
+    }
+
+    public override void Grow() {
+        base.Grow();
+        renderer.sprite = animationFrames[Mathf.FloorToInt(animationFrames.Length * distCovered)]; // Change sprite based on point in sequence. (I.E. With 5 sprites it changes to the next one every 1 / 5 = 0.2 so it will change sprites at 0.2 , 0.4, 0.6, 0.8 etc in a linear fashion.
     }
 }
