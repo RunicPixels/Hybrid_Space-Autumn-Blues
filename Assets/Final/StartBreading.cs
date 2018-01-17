ff358db403fb6185e86007a6c9bb957c10dcbe30
b <bas.de.waal@hotmail.com>
2018-01-17 22:30:21 +0100

    dsa

--- Assets/Final/StartBreading.cs
+++ Assets/Final/StartBreading.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 public class StartBreading : MonoBehaviour {
 
     public Texture2D fadeOutTexture;
-    public float fadeSpeed = 1.5f;
+    public float fadeSpeed = 0.8f;
     int drawDepth = -1000; 
-    float alpha = 1f;
+    float alpha = 1.0f;
     int fadeDir = -1;
     public int currentScene = 0;
 
@@ -103,6 +103,8 @@ public class StartBreading : MonoBehaviour {
 
         alpha += fadeDir * fadeSpeed * Time.deltaTime;  
         alpha = Mathf.Clamp01(alpha);
+        Debug.Log(alpha);
+
         GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
         GUI.depth = drawDepth;
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
@@ -121,13 +123,11 @@ public class StartBreading : MonoBehaviour {
         }
     }
     
-    private IEnumerator ProgressToNextScene()
-    {
+    private IEnumerator ProgressToNextScene(){
         MovementManager.instance.DisableCurrentMovement();
         yield return new WaitForSeconds(5f);
 
-        if (currentScene == 1)
-        {
+        if (currentScene == 1){
             currentScene = 2;
             StartCoroutine(Fade());
         }
