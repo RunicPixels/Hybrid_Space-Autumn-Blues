a78e0461500e73141b98c19f46ec38275e9b9dab
b <bas.de.waal@hotmail.com>
2018-01-18 09:39:35 +0100

    asdasdas

--- Assets/Scripts/StartSceneOrb.cs
+++ Assets/Scripts/StartSceneOrb.cs
@@ -6,7 +6,7 @@ public class StartSceneOrb : MonoBehaviour {
 
     public StartSceneOrb otherOrb;
     public bool active = false;
-    private bool triggered = false
+    private bool triggered = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +16,7 @@ public class StartSceneOrb : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(otherOrb != null)
-        {
+        {   
             if ((active && otherOrb.active) && !triggered)
             {
                 GameObject.Find("DontRemoveItStartsBreathing").GetComponent<StartBreading>().startSceneOne = true;
